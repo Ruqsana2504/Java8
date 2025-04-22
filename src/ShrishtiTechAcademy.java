9510ca62969f300de3152bd1bbfb41e8c7e9b5fe
Ruqsana2504 <ruqsana123begum@gmail.com>
2025-04-22 23:33:11 +0530

    Added queries related to Stream API

--- src/ShrishtiTechAcademy.java
+++ src/ShrishtiTechAcademy.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class ShrishtiTechAcademy {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.List;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class ShrishtiTechAcademy {
+
+    public static void main(String[] args) {
+
+        List<String> courseList = List.of("Java", "Spring", "Angular", "HTML", "Javascript");
+
+        //Iterate over list and find elements which contains 'a'. Return sorted array
+        courseList.stream()
+                .filter(course -> course.contains("a"))
+                .sorted()
+                .forEach(System.out::println);
+
+        System.out.println();
+        courseList.stream()
+                .sorted()
+                .limit(3)
+                .forEach(System.out::println);
+
     }
 }
