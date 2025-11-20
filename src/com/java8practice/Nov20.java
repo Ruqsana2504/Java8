bba1189b7f49d4ca60f1d4e4a4445395dc63534d
Ruqsana <107501777+Ruqsana2504@users.noreply.github.com>
2025-11-20 18:50:18 +0530

    practice

--- src/com/java8practice/Nov20.java
+++ src/com/java8practice/Nov20.java
@@ -1,93 +1,148 @@
 package src.com.java8practice;
 
+import src.com.gdb.Employeee;
+import src.com.javatechie.stream.map.Customer;
+import src.com.javatechie.stream.map.EkartDatabase;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 public class Nov20 {
 
     static void main() {
 
+        List<Employeee> employeeList = Employeee.getEmployees();
+        List<Customer> customerList = EkartDatabase.getAll();
+
+        String input = "Java is is awesome and and interesting";
+        Map<String, Integer> map = Map.of("a", 6, "b", 1, "c", 7, "d", 3);
+        List<Integer> numbers = List.of(9, 12, 1, 25, 4, 3, 10, 9, 1, 4, 9, 12, 25);
+
 //        1. Filter even numbers from a list using streams
-        System.out.println("1. Filter even numbers from a list using streams");
+        List<Integer> evenNumbers = numbers.stream()
+                .filter(num -> num % 2 == 0)
+                .toList();
+        System.out.println("1. Filter even numbers from a list using streams : " + evenNumbers);
+        System.out.println();
 
 //        2. Count word frequency in a given string
-        System.out.println("2. Count word frequency in a given string");
+        Map<String, Long> wordFrequency = Arrays.stream(input.split(" "))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println("2. Count word frequency in a given string : " + wordFrequency);
+        System.out.println();
 
 //        3. Find first non - repeating character
-        System.out.println("3. First non-repeating character");
+        HashSet<Character> hs = new HashSet<>();
+        Character nonRepeatingChar = input.chars()
+                .mapToObj(ch -> (char) ch)
+//                .filter(ch -> ch != ' ')
+//                .filter(c -> !hs.add(c))
+                .filter(ch -> input.indexOf(ch) == input.lastIndexOf(ch))
+                .findFirst()
+                .orElse(null);
+        System.out.println("3. First non-repeating character : " + nonRepeatingChar);
+        System.out.println();
 
 //        4. Remove duplicates from a list
-        System.out.println("4. Remove duplicates from a list");
+        List<String> distinctElements = Arrays.stream(input.split(" "))
+                .distinct()
+                .toList();
+        System.out.println("4. Remove duplicates from a list : " + distinctElements);
+        System.out.println();
 
 //        5. Sort employees by salary using Comparator
-        System.out.println("5.Sort employees by salary using Comparator ");
+        List<Employeee> sortedEmpSalary = employeeList.stream()
+                .sorted(Comparator.comparingDouble(Employeee::getSalary))
+                .toList();
+        System.out.println("5. Sort employees by salary using Comparator : " + sortedEmpSalary);
+        System.out.println();
 
 //        6. Convert a list of strings to uppercase
-        System.out.println("6. Convert a list of strings to uppercase");
+        List<String> upperCaseString = Arrays.stream(input.split(" "))
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println("6. Convert a list of strings to uppercase : " + upperCaseString);
+        System.out.println();
 
-//       7. Sum all numbers in a list using reduce ()
-        System.out.println("7. Sum all numbers in a list using reduce()");
+//       7. Sum all numbers in a list using reduce()
+        int sum = numbers.stream()
+//                .reduce(0, (a, b) -> a + b);
+                .reduce(0, Integer::sum);
+        System.out.println("7. Sum all numbers in a list using reduce() : " + sum);
+        System.out.println();
 
 //       8. Join strings into a comma-separated value
-        System.out.println("8. Join strings into a comma-separated value");
+        String joinWords = Arrays.stream(input.split(" "))
+                .collect(Collectors.joining(","));
+        System.out.println("8. Join strings into a comma-separated value : " + joinWords);
+        System.out.println();
 
 //        9. Partition list into even/odd using Collectors.partitioningBy()
         System.out.println("9. Partition list into even/odd using Collectors.partitioningBy()");
+        Map<Boolean, List<Integer>> partitionedElements = numbers.stream()
+                .collect(Collectors.partitioningBy(num -> num % 2 == 0));
+        System.out.println("9.1. Even Numbers : " + partitionedElements.get(true));
+        System.out.println("9.2. Odd Numbers : " + partitionedElements.get(false));
+        System.out.println();
 
 //        10. Find the second-highest number in a list
-        System.out.println("10. Find the second highest number in a list");
+        int secondHighest = numbers.stream()
+                .distinct()
+                .sorted(Comparator.reverseOrder())
+                .skip(1)
+                .findFirst()
+                .orElse(0);
+        System.out.println("10. Find the second highest number in a list : " + secondHighest);
+        System.out.println();
 
 //        11. Flatten a list of lists using flatMap()
-        System.out.println("11. Flatten a list of lists using flatMap()");
+        List<String> flatMapString = customerList.stream()
+//                .map(customer -> customer.getPhoneNumbers())
+                .flatMap(customer -> customer.getPhoneNumbers().stream())
+                .toList();
+        System.out.println("11. Flatten a list of lists using flatMap() : " + flatMapString);
+        System.out.println();
 
 //        12. Count elements in each category(e.g.department → count)
-        System.out.println("12. Count elements in each category");
+        Map<String, Long> deptCount = employeeList.stream()
+                .collect(Collectors.groupingBy(Employeee::getDepartment, Collectors.counting()));
+        System.out.println("12. Count elements in each category : " + deptCount);
+        System.out.println();
 
 //        13. Sort a Map by its values
-        System.out.println("13. Sort a Map by its values");
+        Map<String, Integer> sortedMapByValue = map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> a, LinkedHashMap::new));
+        System.out.println("13. Sort a Map by its values : " + sortedMapByValue);
+        System.out.println();
 
-//        14. Filter list based on multiple conditions
-        System.out.println("14. Filter list based on multiple conditions");
+//        14. Find duplicates in a list using streams
+        hs.clear();
+        Set<Character> duplicateSet = input.chars()
+                .mapToObj(ch -> (char) ch)
+                .filter(ch -> !hs.add(ch))
+                .collect(Collectors.toSet());
+        System.out.println("14. Find duplicates in a list using streams : " + duplicateSet);
+        System.out.println();
 
-//        15. Find duplicates in a list using streams
-        System.out.println("15. Find duplicates in a list using streams");
+//        15. Write a custom collector to join strings in uppercase
+        Collector<String, ?, String> upperJoiner =
+                Collectors.collectingAndThen(
+                        Collectors.mapping(String::toUpperCase, Collectors.joining(",")),
+                        s -> s
+                );
+        String result = Arrays.stream(input.split(" ")).collect(upperJoiner);
+        System.out.println("15. Write a custom collector to join strings in uppercase : " + result);
+        System.out.println();
 
-//        16. Remove nulls or empty strings
-        System.out.println("16. Remove nulls or empty strings");
-
-//        17. Use peek () for stream debugging
-        System.out.println("17. Use peek () for stream debugging");
-
-//        18. Group employees by department using Collectors.groupingBy()
-        System.out.println("18. Group employees by department using Collectors.groupingBy()");
-
-//        19. Implement caching with ConcurrentHashMap + computeIfAbsent()
-        System.out.println("19. Implement caching with ConcurrentHashMap + computeIfAbsent()");
-
-//        20. Write a custom collector to join strings in uppercase
-        System.out.println("20. Write a custom collector to join strings in uppercase");
-
-//        21. Use Optional for null - safe access to nested objects
-        System.out.println("21. Use Optional for null - safe access to nested objects");
-
-//        22. Stream large files line -by - line efficiently
-        System.out.println("22. Stream large files line -by - line efficiently");
-
-//        23. Write a generic method using Function<T, R >
-        System.out.println("23. Write a generic method using Function<T, R >");
-
-//        24. Chain Optional and Stream APIs in service layers
-        System.out.println("24. Chain Optional and Stream APIs in service layers");
-
-//        25. Handle duplicate keys with Collectors.toMap() and merge function
-        System.out.println("25. Handle duplicate keys with Collectors.toMap() and merge function");
-
-//        26. Compare performance of stream () vs parallelStream ()
-        System.out.println("26. Compare performance of stream () vs parallelStream ()");
-
-//        27. Build a stream - based data processing pipeline(CSV → Filter → Map → Collect)
-        System.out.println(" 27. Build a stream - based data processing pipeline(CSV → Filter → Map → Collect)");
-
-//        28. Find longest-string using max(Comparator.comparingInt(String::length))
-        System.out.println("28. Find longest string using max(Comparator.comparingInt(String::length))");
-
+//        16. Find longest-string using max(Comparator.comparingInt(String::length))
+        String longestString = Arrays.stream(input.split(" "))
+                .max(Comparator.comparingInt(String::length))
+                .orElse("");
+        System.out.println("16. Find longest string using max(Comparator.comparingInt(String::length)) : " + longestString);
+        System.out.println();
 
     }
 }
