dcb18b6892c64c62a06dab69877bb26728b9a23c
Ruqsana <107501777+Ruqsana2504@users.noreply.github.com>
2025-11-20 16:44:19 +0530

    Structure for Java-8 practise questions added

---
+++ src/com/java8practice/Nov20.java
@@ -0,0 +1,93 @@
+package src.com.java8practice;
+
+public class Nov20 {
+
+    static void main() {
+
+//        1. Filter even numbers from a list using streams
+        System.out.println("1. Filter even numbers from a list using streams");
+
+//        2. Count word frequency in a given string
+        System.out.println("2. Count word frequency in a given string");
+
+//        3. Find first non - repeating character
+        System.out.println("3. First non-repeating character");
+
+//        4. Remove duplicates from a list
+        System.out.println("4. Remove duplicates from a list");
+
+//        5. Sort employees by salary using Comparator
+        System.out.println("5.Sort employees by salary using Comparator ");
+
+//        6. Convert a list of strings to uppercase
+        System.out.println("6. Convert a list of strings to uppercase");
+
+//       7. Sum all numbers in a list using reduce ()
+        System.out.println("7. Sum all numbers in a list using reduce()");
+
+//       8. Join strings into a comma-separated value
+        System.out.println("8. Join strings into a comma-separated value");
+
+//        9. Partition list into even/odd using Collectors.partitioningBy()
+        System.out.println("9. Partition list into even/odd using Collectors.partitioningBy()");
+
+//        10. Find the second-highest number in a list
+        System.out.println("10. Find the second highest number in a list");
+
+//        11. Flatten a list of lists using flatMap()
+        System.out.println("11. Flatten a list of lists using flatMap()");
+
+//        12. Count elements in each category(e.g.department → count)
+        System.out.println("12. Count elements in each category");
+
+//        13. Sort a Map by its values
+        System.out.println("13. Sort a Map by its values");
+
+//        14. Filter list based on multiple conditions
+        System.out.println("14. Filter list based on multiple conditions");
+
+//        15. Find duplicates in a list using streams
+        System.out.println("15. Find duplicates in a list using streams");
+
+//        16. Remove nulls or empty strings
+        System.out.println("16. Remove nulls or empty strings");
+
+//        17. Use peek () for stream debugging
+        System.out.println("17. Use peek () for stream debugging");
+
+//        18. Group employees by department using Collectors.groupingBy()
+        System.out.println("18. Group employees by department using Collectors.groupingBy()");
+
+//        19. Implement caching with ConcurrentHashMap + computeIfAbsent()
+        System.out.println("19. Implement caching with ConcurrentHashMap + computeIfAbsent()");
+
+//        20. Write a custom collector to join strings in uppercase
+        System.out.println("20. Write a custom collector to join strings in uppercase");
+
+//        21. Use Optional for null - safe access to nested objects
+        System.out.println("21. Use Optional for null - safe access to nested objects");
+
+//        22. Stream large files line -by - line efficiently
+        System.out.println("22. Stream large files line -by - line efficiently");
+
+//        23. Write a generic method using Function<T, R >
+        System.out.println("23. Write a generic method using Function<T, R >");
+
+//        24. Chain Optional and Stream APIs in service layers
+        System.out.println("24. Chain Optional and Stream APIs in service layers");
+
+//        25. Handle duplicate keys with Collectors.toMap() and merge function
+        System.out.println("25. Handle duplicate keys with Collectors.toMap() and merge function");
+
+//        26. Compare performance of stream () vs parallelStream ()
+        System.out.println("26. Compare performance of stream () vs parallelStream ()");
+
+//        27. Build a stream - based data processing pipeline(CSV → Filter → Map → Collect)
+        System.out.println(" 27. Build a stream - based data processing pipeline(CSV → Filter → Map → Collect)");
+
+//        28. Find longest-string using max(Comparator.comparingInt(String::length))
+        System.out.println("28. Find longest string using max(Comparator.comparingInt(String::length))");
+
+
+    }
+}
